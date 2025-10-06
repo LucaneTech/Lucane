f98dddc2766cf44c2ef2b36c6872ab8d93bf4d92
Lucane <contact@lucane.tech>
2025-10-06 23:52:42 +0100

    about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ const About: React.FC = () => {
        <>
         <div className="min-h-screen bg-base-200 flex flex-col items-center justify-center p-4">
             <img src={Logo} alt="Lucane Logo" className="mb-4 w-32 h-32" />
-            <h1 className="text-3xl font-bold">About Us</h1>
+            <h1 className="text-3xl font-bold">Contactez-nous</h1>
             <p className="mt-4">This is a simple React app styled with Tailwind CSS and DaisyUI.</p>
         </div>
        </>
