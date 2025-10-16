ac0fe0948cfe2e5c0d2644be91f0aeb695953f81
Lucane <contact@lucane.tech>
2025-10-16 23:51:06 +0100

    herosection changes on design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
-
+import DarkVeil from "./DarkVeil";
 const Home: React.FC = () => {
-  return(
-       <>
-         {/* Hero Section */}
-        
-          <HeroSection/>
-         
-       </>
+  return (
+    <>
+      {/* Hero Section */}
+
+      <HeroSection />
+
+
+      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+        <DarkVeil />
+      </div>
+
+    </>
   )
 }
 
