b9309ab67030521f5ea82c54b9ce917f4f6a64cc
Lucane <contact@lucane.tech>
2025-10-17 23:55:22 +0100

    changes in about page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
-import DarkVeil from "./DarkVeil";
 const Home: React.FC = () => {
   return (
     <>
       {/* Hero Section */}
 
       <HeroSection />
-
-
-      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
-        <DarkVeil />
-      </div>
 
     </>
   )
