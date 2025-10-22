db7b0de6bcd6978d40ef4975baa5cbdd06f90845
Lucane <contact@lucane.tech>
2025-10-22 22:46:55 +0100

    changes

--- src/pages/Trainings.tsx
+++ src/pages/Trainings.tsx
@@ -7,7 +7,7 @@ const Trainings: React.FC = () => {
        <>
         <div className="min-h-screen bg-base-200 flex flex-col items-center justify-center p-4">
             <img src={Logo} alt="Lucane Logo" className="mb-4 w-32 h-32" />
-            <h1 className="text-3xl font-bold">Welcome to Lucane</h1>
+           
         </div>
        </>
   )
